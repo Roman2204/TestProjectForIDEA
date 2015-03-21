b29bcd69dcdf6cd02d4afebba36dd4371f1dc52d
Roman Alexeev <roman220492@gmail.com>
2015-03-22 01:27:46 +0300

    Добавил 2 новые строки с выводом на экран

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Worlds");
+        System.out.println("Other string");
+        System.out.println("And one other string");
     }
 }
